3409c6cd48b0b8a2f6044692cd3150e350eebdbb
YowuJH <josuejimenezh05@gmail.com>
2026-09-13 08:45:08 -0500

    Logica para asignar el reporte a entidad automaticamente mediante latitud y longitud

--- src/main/java/com/upc/huellasdeauxilio/servicios/EntidadServicio.java
+++ src/main/java/com/upc/huellasdeauxilio/servicios/EntidadServicio.java
@@ -36,4 +36,44 @@ public class EntidadServicio {
     public List<Entidad> buscarPorNombre(String nombre) {
         return entidadRepositorio.findByNombreEntidadContainingIgnoreCase(nombre);
     }
+
+    // logica para encontrar la entidad más cercana
+    public Entidad obtenerEntidadMasCercana(Float latReporte, Float lonReporte) {
+        List<Entidad> todasLasEntidades = entidadRepositorio.findAll();
+        Entidad masCercana = null;
+        double distanciaMinima = Double.MAX_VALUE; // Iniciamos con un número gigante
+
+        for (Entidad entidad : todasLasEntidades) {
+            // Solo calculamos si la entidad tiene sus coordenadas registradas
+            if (entidad.getLatitud() != null && entidad.getLongitud() != null) {
+
+                double distancia = calcularDistancia(latReporte, lonReporte, entidad.getLatitud(), entidad.getLongitud());
+
+                // Si esta distancia es menor que la que teníamos guardada, esta es la nueva más cercana
+                if (distancia < distanciaMinima) {
+                    distanciaMinima = distancia;
+                    masCercana = entidad;
+                }
+            }
+        }
+        return masCercana;
+    }
+
+    private double calcularDistancia(float lat1, float lon1, float lat2, float lon2) {
+        final int RADIO_TIERRA = 6371; // esto es el radio de la tierra en kilómetros
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return RADIO_TIERRA * c; // retorna la distancia en Kilómetros
+    }
+
+
+
 }
