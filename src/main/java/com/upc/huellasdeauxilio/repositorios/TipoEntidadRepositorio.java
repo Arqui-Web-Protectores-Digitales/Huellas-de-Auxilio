7746806ac55851ddc35799a01bda0739df06f1d9
Jefferson <u20241e439@upc.edu.pe>
2026-09-15 01:24:48 -0500

    refactor: simplificar tipo de entidad como atributo

--- src/main/java/com/upc/huellasdeauxilio/repositorios/TipoEntidadRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.upc.huellasdeauxilio.repositorios;
-
-import com.upc.huellasdeauxilio.entidades.TipoEntidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoEntidadRepositorio extends JpaRepository<TipoEntidad, Long> {
-}
